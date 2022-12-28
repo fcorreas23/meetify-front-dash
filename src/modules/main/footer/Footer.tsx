34e873a5e455d1195fd6c4f288e9bebe3eceac6c
fcorreas <fcorreas23@gmail.com>
2022-12-28 19:54:18 -0300

    template dashboar meetify

---
+++ src/modules/main/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {useTranslation} from 'react-i18next';
+import {DateTime} from 'luxon';
+import packageJSON from '../../../../package.json';
+
+const Footer = () => {
+  const [t] = useTranslation();
+
+  return (
+    <footer className="main-footer">
+      <strong>
+        <span>Copyright © {DateTime.now().toFormat('y')} </span>
+      </strong>
+      <div className="float-right d-none d-sm-inline-block">
+        <b>{t<string>('footer.version')}</b>
+        <span>&nbsp;{packageJSON.version}</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
